5b99c1fb212f7b36940a64d2003871a70e24ebb5
karolrewer <122180128+karolrewer@users.noreply.github.com>
2023-01-31 23:55:11 +0100

    Add main variables and listeners in JS file

---
+++ src/javascript.js
@@ -0,0 +1,72 @@
+const currentNumber = document.querySelector('.currentNumber');
+
+const previousNumber = document.querySelector('.previousNumber');
+
+const mathSign = document.querySelector('.mathSign');
+
+const numbersButtons = document.querySelectorAll('.btn');
+
+const operatorsButtons = document.querySelectorAll('.operator');
+
+const equalsButton = document.querySelector('.equals');
+
+const clearButton = document.querySelector('.clear');
+
+const calculatorHistory = document.querySelector('.history');
+
+const historyBtn = document.querySelector('.clearHistory');
+
+
+let result = '';
+
+
+
+function displayNumbers() {
+
+}
+
+function operate() {
+    
+}
+
+function showResult() {
+
+}
+
+function clearScreen() {
+
+}
+
+
+function clearHistory() {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// listeneres
+
+operatorsButtons.forEach((button) => button.addEventListener('click', operate));
+
+equalsButton.addEventListener('click', showResult);
+
+clearButton.addEventListener('click', clearScreen);
+
+numbersButtons.forEach((button) => {
+    button.addEventListener('click', displayNumbers)
+});
+
+historyBtn.addEventListener('click', clearHistory);
